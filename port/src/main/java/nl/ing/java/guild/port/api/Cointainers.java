1c6d17827d82b460bf84f5eea2ce505dd665bbfd
Marek Kapowicki <marek.kapowicki@ing.com>
2018-03-22 11:18:12 +0100

    change the model

--- port/src/main/java/nl/ing/java/guild/port/api/Cointainers.java
+++ port/src/main/java/nl/ing/java/guild/port/api/Cointainers.java
@@ -14,14 +14,35 @@ public class Cointainers {
             .containerId("someId_2")
             .name("Shipment # 2")
             .size("50 ft")
-            .location("Warsaw")
+            .location("Venlo")
             .info("some plums")
             .build();
     static Container mangos = Container.builder()
             .containerId("someId_3")
             .name("Shipment # 3")
             .size("60 ft")
-            .location("Warsaw")
+            .location("Amsterdam")
+            .info("some plums")
+            .build();
+    static Container number1 = Container.builder()
+            .containerId("someId_4")
+            .name("Shipment # 4")
+            .size("60 ft")
+            .location("Brussels")
+            .info("some plums")
+            .build();
+    static Container number2 = Container.builder()
+            .containerId("someId_5")
+            .name("Shipment # 5")
+            .size("30 ft")
+            .location("Bruges")
+            .info("some plums")
+            .build();
+    static Container number3 = Container.builder()
+            .containerId("someId_6")
+            .name("Shipment # 6")
+            .size("40 ft")
+            .location("Delft")
             .info("some plums")
             .build();
 }
--- port/src/main/java/nl/ing/java/guild/port/api/PortController.java
+++ port/src/main/java/nl/ing/java/guild/port/api/PortController.java
@@ -10,6 +10,9 @@ import java.util.List;
 
 import static nl.ing.java.guild.port.api.Cointainers.apples;
 import static nl.ing.java.guild.port.api.Cointainers.mangos;
+import static nl.ing.java.guild.port.api.Cointainers.number1;
+import static nl.ing.java.guild.port.api.Cointainers.number2;
+import static nl.ing.java.guild.port.api.Cointainers.number3;
 import static nl.ing.java.guild.port.api.Cointainers.plums;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -22,6 +25,10 @@ public class PortController {
         Shipment applesShipment = new Shipment("ashish.jain@ing.com", apples);
         Shipment plumShipment = new Shipment("ashish.jain@ing.com", plums);
         Shipment mangoShipment = new Shipment("ashish.jain@ing.com", mangos);
-        return Lists.newArrayList(applesShipment, plumShipment,mangoShipment);
+        Shipment number1Shipment = new Shipment("ashish.jain@ing.com", number1);
+        Shipment number2Shipment = new Shipment("ashish.jain@ing.com", number2);
+        Shipment number3Shipment = new Shipment("ashish.jain@ing.com", number3);
+
+        return Lists.newArrayList(applesShipment, plumShipment,mangoShipment,number1Shipment,number2Shipment,number3Shipment);
     }
 }
